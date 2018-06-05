c4730d7027f8f5135c3cdc522a179384d3f7cff5
Maxcell Wilson <maxcell.wilson@gmail.com>
2018-06-05 15:16:06 -0400

    Add comments to code

--- 01_hashes_and_the_internet/app.rb
+++ 01_hashes_and_the_internet/app.rb
@@ -12,23 +12,36 @@ require 'pry'
 #
 # get_reddit
 
+
+# A Welcome message to show our uses
 def welcome
   puts "Welcome to your library application"
 end
 
+# Ask them what term they want to search and then store
+# whatever the user inputted into a variable to get returned
+# from our method
 def get_term
   puts "What do you want search for? "
   term = gets.chomp
-  # binding.pry
 end
 
+# Here we make our network request, our request to the Google Books API
+# We will go out to the Internet using the RestClient and it will store
+# the response from the data inside of response. What we get back is a <RestClient::Response>
+# We will use JSON.parse to take the response's body and convert it into
+# a hash that Ruby can use
 def search_for_term(term)
   response = RestClient.get "https://www.googleapis.com/books/v1/volumes?q={#{term}}"
   json = JSON.parse(response.body)
-
   json
 end
 
+# Here we take the method from above and call it within this method
+# We will store its results within in a variable `json`. From there we will
+# make a new array of hashes where we just put in the information of `title`, `publisher`, `publishedDate`
+# into it. And last but not least, we are gonna take it through the array and for **each** one
+# we will display a set of information.
 def display_books(term)
   json = search_for_term(term)
 
@@ -51,8 +64,4 @@ end
 
 welcome
 term = get_term
-# puts
 display_books(term)
-
-
-# https://www.googleapis.com/books/v1/volumes?q={}
